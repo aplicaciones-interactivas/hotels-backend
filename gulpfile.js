8449c366f75abc7ccc84a449119409ad0ec9f914
Eduardo <hernandezed.1991@gmail.com>
2019-08-27 01:11:31 -0300

    add docker?

--- gulpfile.js
+++ gulpfile.js
@@ -1,16 +1,18 @@
 const gulp = require('gulp');
 const mocha = require('gulp-mocha');
 const nodemon = require('gulp-nodemon');
-
+const dockerize = require('./config/TestContainer');
 gulp.task('test', (cb) => {
     process.env.NODE_ENV = 'test';
-    gulp.src(['test/**/*.js'], {read: false})
-        .pipe(mocha({reporter: 'list', exit: true}))
-        .on('error', console.error)
-        .once('end', () => {
-            process.exit();
-        });
-    cb();
+    dockerize(() => {
+        gulp.src(['test/**/*.js'], {read: false})
+            .pipe(mocha({reporter: 'list', exit: true}))
+            .on('error', console.error)
+            .once('end', () => {
+                process.exit();
+            });
+
+    }, cb);
 });
 
 gulp.task('dev', (cb) => {
